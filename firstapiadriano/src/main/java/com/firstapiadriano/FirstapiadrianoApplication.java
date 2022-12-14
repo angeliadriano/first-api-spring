edae408cb36d3b37771790fd917a1e3798874ab8
Adriano Angeli <adrianoangeli01@gmail.com>
2022-12-14 13:46:14 -0300

    The First Commit on GitHub

---
+++ firstapiadriano/src/main/java/com/firstapiadriano/FirstapiadrianoApplication.java
@@ -0,0 +1,13 @@
+package com.firstapiadriano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstapiadrianoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstapiadrianoApplication.class, args);
+	}
+
+}
---
+++ firstapiadriano/src/test/java/com/firstapiadriano/FirstapiadrianoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstapiadriano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstapiadrianoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
